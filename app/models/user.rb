919769e9662e4c3a5d0390b2e2d2d37aef1e2402
SkipEveryLunch <skipeverylunch@outlook.jp>
2022-09-18 17:48:58 +0900

    feat/implement email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
+require "validator/email_validator"
+
 class User < ApplicationRecord
+    before_validation :downcase_email
+
     has_secure_password
     validates :name, presence: true,
         length: { maximum: 30, allow_blank: true }
     VALID_PASSWORD_REGEX = /\A[\w\-]+\z/
+    validates :email, presence: true,
+        email: {allow_blank:true}
     validates :password, presence: true,
         length: { minimum: 8 },
         format: {
@@ -11,4 +17,20 @@ class User < ApplicationRecord
             allow_blank: true
         },
         allow_nil: true
+
+    class << self
+        def find_by_activated(email)
+            find_by(email: email, activated: true)
+        end
+    end
+
+    def email_activated?
+        users = User.where.not(id: id)
+        users.find_by_activated(email).present?
+    end
+
+    private
+    def downcase_email
+        self.email.downcase! if email
+    end
 end
